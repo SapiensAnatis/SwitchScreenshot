ab0a77727f76418f7f1dfd551e120f9497b539b7
Jay Malhotra <jay0@me.com>
2017-08-04 21:04:25 +0100

    Theoretical sql complete

--- SwitchScreenshot/Program.cs
+++ SwitchScreenshot/Program.cs
@@ -9,9 +9,6 @@ namespace SwitchScreenshot.Main
 {
     class Program
     {
-
-        private static string _Credentials = @"server=localhost;userid=DTBridgeBot;password=GOOD password;database=DTBridgeDB;SslMode=None";
-
         static void Main(string[] args)
         {
             // Initialize database
@@ -20,7 +17,7 @@ namespace SwitchScreenshot.Main
 
             try {
                 Console.WriteLine("Initializing database...");
-                Connection = new MySqlConnection(_Credentials);
+                Connection = new MySqlConnection(Data.Credentials);
                 Connection.Open();
                 Console.WriteLine($"Connection opened. MySQL version: {Connection.ServerVersion}");
 
@@ -28,12 +25,12 @@ namespace SwitchScreenshot.Main
                 MySqlCommand Command = new MySqlCommand("CREATE DATABASE IF NOT EXISTS DTBridge;");
                 Command.Connection = Connection;
                 Command.ExecuteNonQuery();
-                Command = new MySqlCommand("CREATE TABLE IF NOT EXISTS DiscordUsers(Id BIGINT UNSIGNED NOT NULL PRIMARY KEY);");
+                Command.CommandText = "CREATE TABLE IF NOT EXISTS DiscordUsers(Id BIGINT UNSIGNED NOT NULL PRIMARY KEY);";
                 Command.ExecuteNonQuery();
-                Command = new MySqlCommand("CREATE TABLE IF NOT EXISTS TwitterUsers(Id BIGINT UNSIGNED NOT NULL PRIMARY KEY);");
+                Command.CommandText = "CREATE TABLE IF NOT EXISTS TwitterUsers(Id BIGINT UNSIGNED NOT NULL PRIMARY KEY);";
                 Command.ExecuteNonQuery();
                 // Intermediary table
-                Command = new MySqlCommand("CREATE TABLE IF NOT EXISTS DiscordTwitterUsers(DiscordId BIGINT UNSIGNED NOT NULL, TwitterId BIGINT UNSIGNED NOT NULL);");
+                Command.CommandText = "CREATE TABLE IF NOT EXISTS DiscordTwitterUsers(DiscordId BIGINT UNSIGNED NOT NULL, TwitterId BIGINT UNSIGNED NOT NULL);";
                 Command.ExecuteNonQuery();
             } catch (MySqlException e) {
                 Console.WriteLine($"SQLError {e.ToString()}");
@@ -53,15 +50,19 @@ namespace SwitchScreenshot.Main
             Thread DiscordThread = new Thread(SwitchScreenshot.Discord.DiscordBot.Init);
             DiscordThread.Start();
         }
+    }
+    static class Data 
+    {
+        public static string Credentials = @"server=localhost;userid=DTBridgeBot;password=GOOD password;database=DTBridgeDB;SslMode=None";
 
-        static List<ulong> GetSubscribedUsers(ulong twitterId)
+        public static List<ulong> GetSubscribedUsers(ulong twitterId)
         {
             MySqlConnection Connection = null;
             MySqlDataReader Reader = null;
             List<ulong> Results = new List<ulong>();
 
             try {
-                Connection = new MySqlConnection(_Credentials);
+                Connection = new MySqlConnection(Credentials);
                 Connection.Open();
                 MySqlCommand Command = new MySqlCommand("SELECT DiscordId FROM DiscordTwitterUsers WHERE TwitterId=@twitterId", Connection);
                 Command.Prepare();
@@ -82,6 +83,39 @@ namespace SwitchScreenshot.Main
             return Results;
             
             // Can select additional data from DiscordUsers with this ID if we want, but not for now.
+        }
+
+        public static void SubscribeUser(ulong DiscordUserId, string TwitterUsername)
+        {
+            // TODO: query Twitter side for twitter ID from @whatever
+            // Until then placeholders for my theoretical SQL
+            ulong TwitterUserId = 0032309376230967;
+            
+            MySqlConnection Connection = null;
+            try {
+                Connection = new MySqlConnection(Credentials);
+                Connection.Open();
+                MySqlCommand Command = new MySqlCommand("INSERT INTO DiscordUsers(Id) VALUES(@Id)");
+                Command.Connection = Connection;
+                Command.Prepare();
+                Command.Parameters.AddWithValue("@Id", DiscordUserId);
+                Command.ExecuteNonQuery();
+
+                Command.CommandText = "INSERT INTO TwitterUsers(Id) VALUES (@Id)";
+                Command.Prepare();
+                Command.Parameters.AddWithValue("@Id", TwitterUserId);
+                Command.ExecuteNonQuery();
+
+                Command.CommandText = "INSERT INTO DiscordTwitterUsers(DiscordId, TwitterId) VALUES(@DiscordId, @TwitterId)";
+                Command.Prepare();
+                Command.Parameters.AddWithValue("@DiscordId", DiscordUserId);
+                Command.Parameters.AddWithValue("@TwitterId", TwitterUserId);
+                Command.ExecuteNonQuery();
+            } catch (MySqlException e) {
+                Console.WriteLine($"Error occured while updating DB records for recently registered user: {e.ToString()}");
+            } finally {
+                if (Connection != null) Connection.Close();
+            }
         }
     }
 }
